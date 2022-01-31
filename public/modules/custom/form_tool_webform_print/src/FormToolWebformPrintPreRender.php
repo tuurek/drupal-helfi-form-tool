1e34df65e85698ff73e481f921f45feb18324a61
Petri Leinonen <petri.leinonen@siili.com>
2022-01-31 08:22:23 +0200

    LOM-18: esitietolomake to its own module

---
+++ public/modules/custom/form_tool_webform_print/src/FormToolWebformPrintPreRender.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\form_tool_webform_print;
+
+use Drupal\Core\Security\TrustedCallbackInterface;
+
+/**
+ * Implements trusted prerender callbacks for the Webform share module.
+ *
+ * @internal
+ */
+class FormToolWebformPrintPreRender implements TrustedCallbackInterface {
+
+  /**
+   * Prerender callback for page.
+   */
+  public static function page($element) {
+    if (!FormToolWebformPrintHelper::isPage()) {
+      return $element;
+    }
+
+    // Remove all theme wrappers from the page template.
+    $element['#theme_wrappers'] = [];
+    return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function trustedCallbacks() {
+    return ['page'];
+  }
+
+}
