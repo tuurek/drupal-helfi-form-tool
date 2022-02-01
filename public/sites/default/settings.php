fed2f1c006afc3441702518f0e6c62275d471ce1
Janne Suominen <janne.suominen@siili.com>
2022-02-01 09:58:45 +0200

    LOM-146: Add helfi_azure_fs to fix permission errors in files folder.

--- public/sites/default/settings.php
+++ public/sites/default/settings.php
@@ -28,6 +28,13 @@ if ($ssl_ca_path = getenv('AZURE_SQL_SSL_CA_PATH')) {
     \PDO::MYSQL_ATTR_SSL_CA => $ssl_ca_path,
     \PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => FALSE,
   ];
+  // Azure specific filesystem fixes.
+  $settings['php_storage']['twig']['directory'] = '/tmp';
+  $settings['php_storage']['twig']['secret'] = $settings['hash_salt'];
+  $settings['file_chmod_directory'] = 16895;
+  $settings['file_chmod_file'] = 16895;
+
+  $config['system.performance']['cache']['page']['max_age'] = 86400;
 }
 
 // Only in Wodby environment.
