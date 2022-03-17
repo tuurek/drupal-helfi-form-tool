cae73fa1d227ec9ef9beee3e1fa33c0a779c5ec5
Janne Suominen <janne.suominen@siili.com>
2022-03-17 12:55:34 +0200

    LOM-176: Add Profile data functionality.

---
+++ public/modules/custom/form_tool_webform_components/form_tool_profile_data/src/Element/FormToolProfileData.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace Drupal\form_tool_profile_data\Element;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\webform\Element\WebformCompositeBase;
+
+/**
+ * Provides a 'webform_example_composite'.
+ *
+ * Webform composites contain a group of sub-elements.
+ *
+ *
+ * IMPORTANT:
+ * Webform composite can not contain multiple value elements (i.e. checkboxes)
+ * or composites (i.e. webform_address)
+ *
+ * @FormElement("form_tool_profile_data")
+ *
+ * @see \Drupal\webform\Element\WebformCompositeBase
+ * @see \Drupal\form_tool_profile_data\Element\FormToolProfileData
+ */
+class FormToolProfileData extends WebformCompositeBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getInfo() {
+    return parent::getInfo() + ['#theme' => 'form_tool_profile_data'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getCompositeElements(array $element) {
+    $elements = [];
+
+    /** @var \Drupal\helfi_helsinki_profiili\HelsinkiProfiiliUserData $hpud */
+    $hpud = \Drupal::service('helfi_helsinki_profiili.userdata');
+
+    $options = \Drupal\form_tool_profile_data\Plugin\WebformElement\FormToolProfileData::getFieldSelections();
+
+    $userData = $hpud->getUserData();
+    $userProfile = $hpud->getUserProfileData();
+
+    if (isset($element['#strong'])) {
+
+      $selectedFields = $element['#strong'];
+
+      if ($userProfile === NULL) {
+        return $elements;
+      }
+
+      if (isset($selectedFields['verifiedFirstName']) && $selectedFields['verifiedFirstName'] !== 0) {
+        $elements['verifiedFirstName'] = [
+          '#type' => 'textfield',
+          '#title' => $options['strong']['verifiedFirstName'],
+          '#value' => $userProfile["myProfile"]["verifiedPersonalInformation"]["firstName"],
+          '#attributes' => ['readonly' => 'readonly'],
+        ];
+      }
+      if (isset($selectedFields['verifiedLastName']) && $selectedFields['verifiedLastName'] !== 0) {
+        $elements['verifiedLastName'] = [
+          '#type' => 'textfield',
+          '#title' => $options['strong']['verifiedLastName'],
+          '#value' => $userProfile["myProfile"]["verifiedPersonalInformation"]["lastName"],
+          '#attributes' => ['readonly' => 'readonly'],
+        ];
+      }
+      if (isset($selectedFields['verifiedSsn']) && $selectedFields['verifiedSsn'] !== 0) {
+        $elements['verifiedSsn'] = [
+          '#type' => 'textfield',
+          '#title' => $options['strong']['verifiedSsn'],
+          '#value' => $userProfile["myProfile"]["verifiedPersonalInformation"]["nationalIdentificationNumber"],
+          '#attributes' => ['readonly' => 'readonly'],
+        ];
+      }
+      if (isset($selectedFields['verifiedGivenName']) && $selectedFields['verifiedGivenName'] !== 0) {
+        $elements['verifiedGivenName'] = [
+          '#type' => 'textfield',
+          '#title' => $options['strong']['verifiedGivenName'],
+          '#value' => $userProfile["myProfile"]["verifiedPersonalInformation"]["givenName"],
+          '#attributes' => ['readonly' => 'readonly'],
+        ];
+      }
+      if (isset($selectedFields['verifiedPermanentAddress']) && $selectedFields['verifiedPermanentAddress'] !== 0) {
+        $elements['verifiedPermanentAddress'] = [
+          '#type' => 'textfield',
+          '#title' => $options['strong']['verifiedPermanentAddress'],
+          '#value' =>
+          $userProfile["myProfile"]["verifiedPersonalInformation"]["permanentAddress"]["streetAddress"] . ', ' .
+          $userProfile["myProfile"]["verifiedPersonalInformation"]["permanentAddress"]["postalCode"] . ', ' .
+          $userProfile["myProfile"]["verifiedPersonalInformation"]["permanentAddress"]["postOffice"],
+          '#attributes' => ['readonly' => 'readonly'],
+        ];
+      }
+    }
+
+    if (isset($element['#weak'])) {
+      $selectedFields = $element['#weak'];
+
+      if (isset($selectedFields['primaryAddress']) && $selectedFields['primaryAddress'] !== 0) {
+        $elements['primaryAddress'] = [
+          '#type' => 'textfield',
+          '#title' => $options['weak']['primaryAddress'],
+          '#value' =>
+          $userProfile["myProfile"]["primaryAddress"]["address"] . ', ' .
+          $userProfile["myProfile"]["primaryAddress"]["postalCode"] . ', ' .
+          $userProfile["myProfile"]["primaryAddress"]["city"] . ', ' .
+          $userProfile["myProfile"]["primaryAddress"]["countryCode"],
+          '#attributes' => ['readonly' => 'readonly'],
+        ];
+      }
+      if (isset($selectedFields['primaryEmail']) && $selectedFields['primaryEmail'] !== 0) {
+        $elements['primaryEmail'] = [
+          '#type' => 'textfield',
+          '#title' => $options['weak']['primaryEmail'],
+          '#value' => $userProfile["myProfile"]["primaryEmail"]["email"],
+          '#attributes' => ['readonly' => 'readonly'],
+        ];
+      }
+      if (isset($selectedFields['primaryPhone']) && $selectedFields['primaryPhone'] !== 0) {
+        $elements['primaryPhone'] = [
+          '#type' => 'textfield',
+          '#title' => $options['weak']['primaryPhone'],
+          '#value' => $userProfile["myProfile"]["primaryPhone"]["phone"],
+          '#attributes' => ['readonly' => 'readonly'],
+        ];
+      }
+    }
+
+    return $elements;
+  }
+
+  /**
+   * Performs the after_build callback.
+   */
+  public static function afterBuild(array $element, FormStateInterface $form_state) {
+    // Add #states targeting the specific element and table row.
+    preg_match('/^(.+)\[[^]]+]$/', $element['#name'], $match);
+    $composite_name = $match[1];
+    $element['#states']['disabled'] = [
+      [':input[name="' . $composite_name . '[first_name]"]' => ['empty' => TRUE]],
+      [':input[name="' . $composite_name . '[last_name]"]' => ['empty' => TRUE]],
+    ];
+    // Add .js-form-wrapper to wrapper (ie td) to prevent #states API from
+    // disabling the entire table row when this element is disabled.
+    $element['#wrapper_attributes']['class'][] = 'js-form-wrapper';
+    return $element;
+  }
+
+}
---
+++ public/modules/custom/form_tool_webform_components/form_tool_profile_data/src/Plugin/WebformElement/FormToolProfileData.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace Drupal\form_tool_profile_data\Plugin\WebformElement;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\webform\Plugin\WebformElement\WebformCompositeBase;
+use Drupal\webform\WebformSubmissionInterface;
+use JetBrains\PhpStorm\ArrayShape;
+
+/**
+ * Provides a 'form_tool_profile_data' element.
+ *
+ * @WebformElement(
+ *   id = "form_tool_profile_data",
+ *   label = @Translation("Form tool Profile data"),
+ *   description = @Translation("Provides webform component to gather details from helsinki profile."),
+ *   category = @Translation("Helfi"),
+ *   multiline = TRUE,
+ *   composite = TRUE,
+ *   states_wrapper = TRUE,
+ * )
+ *
+ * @see \Drupal\form_tool_profile_data\Element\WebformExampleComposite
+ * @see \Drupal\webform\Plugin\WebformElement\WebformCompositeBase
+ * @see \Drupal\webform\Plugin\WebformElementBase
+ * @see \Drupal\webform\Plugin\WebformElementInterface
+ * @see \Drupal\webform\Annotation\WebformElement
+ */
+class FormToolProfileData extends WebformCompositeBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function defineDefaultProperties() {
+    // Here you define your webform element's default properties,
+    // which can be inherited.
+    //
+    // @see \Drupal\webform\Plugin\WebformElementBase::defaultProperties
+    // @see \Drupal\webform\Plugin\WebformElementBase::defaultBaseProperties
+    return [
+      'noauth' => [],
+      'weak' => [],
+      'strong' => [],
+    ] + parent::defineDefaultProperties();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function form(array $form, FormStateInterface $form_state): array {
+
+    $form = parent::form($form, $form_state);
+
+    $options = self::getFieldSelections();
+
+    $webFormSettings = $this->getWebform()->getThirdPartySettings('form_tool_webform_parameters');
+
+    // $form['element']['title']['#default_value'] = 'Profile fields';
+    $form['element']['title']['#value'] = 'Profile fields';
+
+    if ($webFormSettings['login_type'] === '0') {
+      $form['element']['noauth'] = [
+        '#type' => 'checkboxes',
+        '#title' => t('Fields available with no auth'),
+      ];
+      $form['element']['noauth']['#options'] = $options['noauth'];
+    }
+
+    if ($webFormSettings['login_type'] === '1') {
+      $form['element']['weak'] = [
+        '#type' => 'checkboxes',
+        '#title' => t('Fields available with weak auth'),
+      ];
+      $form['element']['weak']['#options'] = $options['weak'];
+    }
+
+    if ($webFormSettings['login_type'] === '2') {
+      $form['element']['strong'] = [
+        '#type' => 'checkboxes',
+        '#title' => t('Fields available with strong auth'),
+      ];
+      $form['element']['strong']['#options'] = $options['strong'];
+    }
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function formatHtmlItemValue(array $element, WebformSubmissionInterface $webform_submission, array $options = []) {
+    return $this->formatTextItemValue($element, $webform_submission, $options);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function formatTextItemValue(array $element, WebformSubmissionInterface $webform_submission, array $options = []) {
+    $value = $this->getValue($element, $webform_submission, $options);
+
+    $titles = FormToolProfileData::getFieldSelections();
+
+    $lines = [];
+    foreach ($value as $fieldName => $fieldValue) {
+      foreach ($titles as $auth => $fields) {
+        if(isset($fields[$fieldName])){
+          $lines[] = $fields[$fieldName]->render() . ': ' . $fieldValue;
+        }
+      }
+
+    }
+    return $lines;
+  }
+
+  /**
+   * Return fields from profile.
+   *
+   * @return array
+   */
+  public static function getFieldSelections(): array {
+    return [
+      'noauth' => [],
+      'weak' => [
+        'primaryAddress' => t('Primary address'),
+        'primaryEmail' => t('Primary email'),
+        'primaryPhone' => t('Primary phone'),
+      ],
+      'strong' => [
+        'verifiedFirstName' => t('Verified first name'),
+        'verifiedLastName' => t('Verified last name'),
+        'verifiedGivenName' => t('Verified given name'),
+        'verifiedSsn' => t('Verified SSN'),
+        'verifiedPermanentAddress' => t('Verified permanent address'),
+      ],
+    ];
+  }
+
+}
