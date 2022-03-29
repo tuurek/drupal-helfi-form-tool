c6c5f17ada83f8a3a1ee802fe6e2765ec130b8e6
Petri Leinonen <petri.leinonen@siili.com>
2022-03-29 12:45:00 +0300

    LOM-176: translations

--- docker/openshift/entrypoints/90-deploy.sh
+++ docker/openshift/entrypoints/90-deploy.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 cd /var/www/html/public && drush deploy
+
+# allow translations to fail
+cd /var/www/html/public && drush helfi:locale-import form_tool_contact_info || true
+cd /var/www/html/public && drush helfi:locale-import form_tool_profile_data || true
+cd /var/www/html/public && drush helfi:locale-import form_tool_webform_parameters || true
