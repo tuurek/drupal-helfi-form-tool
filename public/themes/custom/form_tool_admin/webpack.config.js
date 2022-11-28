35db81b305882fd87aa23edd062dc60a9f358771
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2022-11-28 18:20:43 +0200

    Updated node version, webpack and webpack dependencies for the form tool admin theme.

--- public/themes/custom/form_tool_admin/webpack.config.js
+++ public/themes/custom/form_tool_admin/webpack.config.js
@@ -1,28 +1,153 @@
-const isDev = (process.env.NODE_ENV !== "production");
+const path = require('path');
+const glob = require('glob');
 
-const path = require("path");
-const globImporter = require("node-sass-glob-importer");
+const CopyPlugin = require('copy-webpack-plugin');
+const FriendlyErrorsWebpackPlugin = require('@nuxt/friendly-errors-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+const { merge } = require('webpack-merge');
 
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
-const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
+// Handle entry points.
+const Entries = () => {
+  let entries = {
+    styles: ['./src/scss/styles.scss'],
+  };
 
-module.exports = {
-  entry: {
-    styles: ["./src/scss/styles.scss"],
-    bundle: ["./src/js/common.js"],
-  },
-  watchOptions: {
-    aggregateTimeout: 300,
-  },
-  // Tell us only about the errors.
-  stats: 'errors-only',
-  // Suppress performance errors.
-  performance: {
-    hints: false,
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000
+  const pattern = './src/js/**/*.js';
+  const ignore = [];
+
+  glob.sync(pattern, {ignore: ignore}).map((item) => {
+    entries[path.parse(item).name] = item }
+  );
+  return entries;
+};
+
+module.exports = (env, argv) => {
+
+  const isDev = (argv.mode === 'development');
+
+  // Set the base config
+  const config = {
+    entry() {
+      return Entries();
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      chunkFilename: 'js/async/[name].chunk.js',
+      pathinfo: isDev,
+      filename: 'js/[name].min.js',
+      publicPath: '../',
+      clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+          type: 'javascript/auto',
+        },
+        {
+          test: /\.(css|sass|scss)$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: isDev,
+                importLoaders: 2,
+                esModule: false,
+              },
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                'postcssOptions': {
+                  'config': path.join(__dirname, 'postcss.config.js'),
+                },
+                sourceMap: isDev,
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: isDev,
+                additionalData: "$debug_mode: " + isDev + ";",
+              },
+            },
+          ],
+          type: 'javascript/auto',
+        },
+      ],
+    },
+    resolve: {
+      modules: [
+        path.join(__dirname, 'node_modules'),
+      ],
+      extensions: ['.js', '.json'],
+    },
+    plugins: [
+      new FriendlyErrorsWebpackPlugin(),
+      new RemoveEmptyScriptsPlugin(),
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].min.css',
+      }),
+    ],
+    watchOptions: {
+      aggregateTimeout: 300,
+    },
+    // Tell us only about the errors.
+    stats: 'errors-only',
+    // Suppress performance errors.
+    performance: {
+      hints: false,
+      maxEntrypointSize: 512000,
+      maxAssetSize: 512000
+    }
+  };
+
+
+  if (argv.mode === 'production') {
+    const TerserPlugin = require('terser-webpack-plugin');
+
+    const full_config = merge(config, {
+      mode: 'production',
+      devtool: false,
+      optimization: {
+        minimize: true,
+        minimizer: [
+          new TerserPlugin({
+            terserOptions: {
+              ecma: 2015,
+              format: {
+                comments: false,
+              },
+            },
+            extractComments: false,
+          }),
+        ],
+      },
+    });
+
+    return full_config;
+
+  } else if (argv.mode === 'development') {
+    const SourceMapDevToolPlugin = require('webpack/lib/SourceMapDevToolPlugin');
+
+    const full_config = merge(config, {
+      mode: 'development',
+      devtool: 'eval-source-map',
+      plugins: [
+        new SourceMapDevToolPlugin({
+          filename: '[file].map',
+          exclude: [/node_modules/, /images/, /spritemap/, /svg-sprites/],
+        })
+      ]
+    });
+
+    return full_config;
+
   }
 };
